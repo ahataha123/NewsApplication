83bed9dc6a808e93b0393a5b7c89c006663807d0
ahataha123 <84542936+ahataha123@users.noreply.github.com>
2023-05-17 00:26:28 +0200

    DB and API in progress

---
+++ app/src/main/java/com/example/newsapplication/utilities/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.newsapplication.utilities
+
+class Constants {
+    companion object {
+        const val API_KEY = "6fde9b428e2b4b759532b19d5451ae8c"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+    }
+}
